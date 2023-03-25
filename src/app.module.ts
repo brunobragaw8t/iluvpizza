2bd773f62e7bc6a927b8040bdee88a0ca9c28adb
Bruno Braga <brunobragaw8t@gmail.com>
2023-03-25 14:01:31 +0000

    02-15

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PizzasController } from './pizzas/pizzas.controller';
-import { PizzasService } from './pizzas/pizzas.service';
+import { PizzasModule } from './pizzas/pizzas.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, PizzasController],
-  providers: [AppService, PizzasService],
+  imports: [PizzasModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
