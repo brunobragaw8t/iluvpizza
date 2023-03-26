792537938e647f1152ed3b27250152bb8cf885a5
Bruno Braga <brunobragaw8t@gmail.com>
2023-03-26 14:01:43 +0100

    09-77: establish connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PizzasModule } from './pizzas/pizzas.module';
 
 @Module({
-  imports: [PizzasModule],
+  imports: [
+    PizzasModule,
+    MongooseModule.forRoot(
+      'mongo_db_conncetion',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
