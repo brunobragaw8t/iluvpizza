c9f753bd36ef5c62ca51fb3ac0788459dc1f02d5
Bruno Braga <brunobragaw8t@gmail.com>
2023-03-26 14:07:48 +0100

    09-76: setup PizzaSchema

--- src/pizzas/entities/pizza.entity.ts
+++ src/pizzas/entities/pizza.entity.ts
@@ -1,6 +1,17 @@
-export class Pizza {
-  id: number;
+import { Prop, Schema } from '@nestjs/mongoose';
+import { SchemaFactory } from '@nestjs/mongoose/dist';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pizza extends Document {
+  @Prop()
   name: string;
+
+  @Prop()
   brand: string;
+
+  @Prop([String])
   flavors: string[];
 }
+
+export const PizzaSchema = SchemaFactory.createForClass(Pizza);
--- src/pizzas/pizzas.module.ts
+++ src/pizzas/pizzas.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose/dist';
+import { Pizza, PizzaSchema } from './entities/pizza.entity';
 import { PizzasController } from './pizzas.controller';
 import { PizzasService } from './pizzas.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Pizza.name, schema: PizzaSchema }]),
+  ],
   controllers: [PizzasController],
   providers: [PizzasService],
 })
