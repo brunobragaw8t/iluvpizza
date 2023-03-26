c3a3d2e9c9a53689d3df3d480dbdaae373c71833
Bruno Braga <brunobragaw8t@gmail.com>
2023-03-26 17:16:04 +0100

    Move MongoDB connection string to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PizzasModule } from './pizzas/pizzas.module';
 import { PizzaRatingModule } from './pizza-rating/pizza-rating.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION),
     PizzasModule,
-    MongooseModule.forRoot(
-      'mongo_db_conncetion',
-    ),
     PizzaRatingModule,
   ],
   controllers: [AppController],
