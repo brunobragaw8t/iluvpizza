7a7a1e92c0e482a6edd7590f0f2dc33225012cba
Bruno Braga <brunobragaw8t@gmail.com>
2023-04-09 10:54:32 +0100

    07-62: Fix DTOs in Swagger

--- src/pizzas/dto/update-pizza.dto.ts
+++ src/pizzas/dto/update-pizza.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreatePizzaDto } from './create-pizza.dto';
 
 export class UpdatePizzaDto extends PartialType(CreatePizzaDto) {}
