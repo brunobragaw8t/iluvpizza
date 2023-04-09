a4efd1273cd95b3be19ae84c4a8b6105951b7272
Bruno Braga <brunobragaw8t@gmail.com>
2023-04-09 10:57:57 +0100

    07-63: Add property descriptions to Swagger

--- src/pizzas/dto/create-pizza.dto.ts
+++ src/pizzas/dto/create-pizza.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreatePizzaDto {
+  @ApiProperty({ description: 'The name of the pizza.' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'The brand of the pizza.' })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({ example: [] })
   @IsString({ each: true })
   readonly flavors: string[];
 }
